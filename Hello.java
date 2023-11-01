867230871e9e2e89486053678e907f8a4d20b5a1
quylecse <149578796+quylecse@users.noreply.github.com>
2023-11-01 17:30:09 +0100

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Have a good day!");
+		System.out.println("Add more lines at the same time to see conflicts")
 	}
 }
