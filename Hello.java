77a7ecd081293a4e013b2a6b299b37e283e95f76
quylecse <149578796+quylecse@users.noreply.github.com>
2023-11-01 17:41:24 +0100

    resolve conflict

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ public class Hello {
 		System.out.println("Hello World");
 		System.out.println("Have a good day!");
 		System.out.println("Add more lines at the same time to see conflicts")
+		System.out.println("Everything is now Okay!")
 	}
 }
